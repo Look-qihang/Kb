b7fd9d0c998e47c8f9022b1922d0ba38c6682ff2
KNG <274524285@qq.com>
2024-03-07 15:17:42 +0800

    变量提交

---
+++ CsharpDemo/1-变量/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_变量
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            float b = 1.1f;
+            double c = 1.1111;
+            char d = '1';
+            byte e = 100;
+            Console.WriteLine($"整数：{a}");
+            Console.WriteLine($"浮点数：{b}");
+            Console.WriteLine($"双浮点数：{c}");
+            Console.WriteLine($"字符：{d}");
+            Console.WriteLine($"字节：{e}");
+        }
+    }
+}
---
+++ CsharpDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
